8da3c1c2878ae2412fea4d79aaa218110e0edd83
salvadormaurilio <salvadoredgar99@gmail.com>
2025-09-02 17:08:02 -0600

    Unit tests were added for the AuthRemoteDataSource.

---
+++ app/src/test/java/com/example/baubapchallenge/data/datasource/AuthRemoteDataShould.kt
@@ -0,0 +1,103 @@
+import com.example.baubapchallenge.core.assertThatEquals
+import com.example.baubapchallenge.core.assertThatIsInstanceOf
+import com.example.baubapchallenge.data.datasource.AuthRemoteDataSourceImpl
+import com.example.baubapchallenge.data.datasource.firebase.FirestoreClient
+import com.example.baubapchallenge.data.exception.AuthException
+import com.example.baubapchallenge.fakedata.ANY_PASSWORD
+import com.example.baubapchallenge.fakedata.ANY_PASSWORD_HASHED
+import com.example.baubapchallenge.fakedata.ANY_USER_CURP
+import com.example.baubapchallenge.fakedata.ANY_USER_ID
+import com.example.baubapchallenge.fakedata.ANY_USER_PHONE
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.any
+import org.mockito.kotlin.never
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+
+@RunWith(MockitoJUnitRunner::class)
+@ExperimentalCoroutinesApi
+class AuthRemoteDataSourceImplTest {
+
+    @Mock
+    private lateinit var client: FirestoreClient
+
+    private lateinit var authRemoteDataSource: AuthRemoteDataSourceImpl
+
+    @Before
+    fun setUp() {
+        authRemoteDataSource = AuthRemoteDataSourceImpl(client)
+    }
+
+    @Test
+    fun `Get UserAlreadyExistException when the user exist and signUp is success`() = runTest {
+        whenever(client.userExistsByPhoneOrCurp(ANY_USER_PHONE, ANY_USER_CURP)).thenReturn(true)
+
+        val result = authRemoteDataSource.signUp(ANY_USER_PHONE, ANY_USER_CURP, ANY_PASSWORD).firstOrNull()
+
+        verify(client).userExistsByPhoneOrCurp(ANY_USER_PHONE, ANY_USER_CURP)
+        verify(client, never()).createUser(any(), any(), any())
+        assertThatIsInstanceOf<AuthException.UserAlreadyExistException>(result?.exceptionOrNull())
+    }
+
+    @Test
+    fun `Get SignUpException when the user exist and signUp is failure`() = runTest {
+        whenever(client.userExistsByPhoneOrCurp(ANY_USER_PHONE, ANY_USER_CURP)).thenThrow(RuntimeException())
+
+        val result = authRemoteDataSource.signUp(ANY_USER_PHONE, ANY_USER_CURP, ANY_PASSWORD).firstOrNull()
+
+        verify(client).userExistsByPhoneOrCurp(ANY_USER_PHONE, ANY_USER_CURP)
+        verify(client, never()).createUser(any(), any(), any())
+        assertThatIsInstanceOf<AuthException.SignUpException>(result?.exceptionOrNull())
+    }
+
+    @Test
+    fun `Get userId when the user does not exist and signUp is success`() = runTest {
+        whenever(client.userExistsByPhoneOrCurp(ANY_USER_PHONE, ANY_USER_CURP)).thenReturn(false)
+        whenever(client.createUser(ANY_USER_PHONE, ANY_USER_CURP, ANY_PASSWORD_HASHED)).thenReturn(ANY_USER_ID)
+
+        val result = authRemoteDataSource.signUp(ANY_USER_PHONE, ANY_USER_CURP, ANY_PASSWORD).firstOrNull()
+
+        verify(client).userExistsByPhoneOrCurp(ANY_USER_PHONE, ANY_USER_CURP)
+        verify(client).createUser(ANY_USER_PHONE, ANY_USER_CURP, ANY_PASSWORD_HASHED)
+        assertThatEquals(ANY_USER_ID, result?.getOrNull())
+    }
+
+    @Test
+    fun `Get SignUpException when the user does not exist and signUp is failure`() = runTest {
+        whenever(client.userExistsByPhoneOrCurp(ANY_USER_PHONE, ANY_USER_CURP)).thenReturn(false)
+        whenever(client.createUser(ANY_USER_PHONE, ANY_USER_CURP, ANY_PASSWORD_HASHED)).thenThrow(RuntimeException())
+
+        val result = authRemoteDataSource.signUp(ANY_USER_PHONE, ANY_USER_CURP, ANY_PASSWORD).firstOrNull()
+
+        verify(client).userExistsByPhoneOrCurp(ANY_USER_PHONE, ANY_USER_CURP)
+        verify(client).createUser(ANY_USER_PHONE, ANY_USER_CURP, ANY_PASSWORD_HASHED)
+        assertThatIsInstanceOf<AuthException.SignUpException>(result?.exceptionOrNull())
+    }
+
+    @Test
+    fun `Get userId when findUserId is success`() = runTest {
+        whenever(client.findUserId(ANY_USER_PHONE, ANY_PASSWORD_HASHED)).thenReturn(ANY_USER_ID)
+
+        val result = authRemoteDataSource.signIn(ANY_USER_PHONE, ANY_PASSWORD).firstOrNull()
+
+        verify(client).findUserId(ANY_USER_PHONE, ANY_PASSWORD_HASHED)
+        assertThatEquals(ANY_USER_ID, result?.getOrNull())
+    }
+
+    @Test
+    fun `Get SignInException when signIn is failure`() = runTest {
+        whenever(client.findUserId(ANY_USER_PHONE, ANY_PASSWORD_HASHED)).thenThrow(RuntimeException())
+
+        val result = authRemoteDataSource.signIn(ANY_USER_PHONE, ANY_PASSWORD).firstOrNull()
+
+        verify(client).findUserId(ANY_USER_PHONE, ANY_PASSWORD_HASHED)
+        assertThatIsInstanceOf<AuthException.SignInException>(result?.exceptionOrNull())
+    }
+}
---
+++ app/src/test/java/com/example/baubapchallenge/fakedata/UserFakeData.kt
@@ -0,0 +1,7 @@
+package com.example.baubapchallenge.fakedata
+
+const val ANY_USER_ID = "MaKNkzR0ySOFAd9PS4IFxnW5rpX2"
+const val ANY_USER_PHONE = "5502098476"
+const val ANY_USER_CURP = "MADX910209HDFRLL09"
+const val ANY_PASSWORD = "Admin1234_1"
+const val ANY_PASSWORD_HASHED = "aa0a7b7bafab98dd708804630febd4eeb79cdd3b8152a6ac856620eca9525050"
